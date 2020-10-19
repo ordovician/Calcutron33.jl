8f8ba2bc715eb8deca4cfb2642b318295c2bb7c5
Erik Engheim <erik.engheim@mac.com>
2020-10-19 02:23:15 +0200

    Added first tests
    Tried running assemble on isa.ct33 and it did not work debugging.

--- test/assem_tests.jl
+++ test/assem_tests.jl
@@ -1,3 +1,10 @@
+datadir = joinpath(dirname(@__FILE__), "data")
+
 @testset "Assembler tests" begin
-    @test 2 == 3
+    @testset "Only Labels" begin
+        syms = open(readsymtable, joinpath(datadir, "labels-nocode.ct33"))
+        ks = ["alpha", "epsilon", "gamma", "delta", "beta"]
+        Base.haskey(key) = haskey(syms, key)
+        @test all(haskey, ks)
+    end
 end
